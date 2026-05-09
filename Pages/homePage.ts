94016e95f447bf9d87f60161ee91deae00dc600c
mac <99664094+DanielObirije@users.noreply.github.com>
2026-05-09 21:38:29 +0100

    deleted files

--- Pages/homePage.ts
+++
@@ -1,9 +0,0 @@
-import type { Page } from "@playwright/test";
-import { expect } from "@playwright/test";
-import { basePage } from "./basePage";
-
-export class homePage extends basePage {
-  constructor(page: Page) {
-    super(page);
-  }
-}
